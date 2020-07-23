f334ff2623d0f0480b12a61baba9ce66c5b1ce22
Matt Scheetz <mfscheetz@gmail.com>
2020-07-22 22:14:40 -0400

    a word

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ class App {
  
   public listen() {
     this.app.listen(this.port, () => {
-      console.log(`App listening on the port ${this.port}`);
+      console.log(`App listening on port ${this.port}`);
     });
   }
 
